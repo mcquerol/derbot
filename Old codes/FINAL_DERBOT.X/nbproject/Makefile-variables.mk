3a4acce5690f076f7334f2e3a415e9327f2af1eb
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2021-06-29 17:22:21 +0000

    added Old codes and its contents

---
+++ Old codes/FINAL_DERBOT.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FINAL_DERBOT.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FINAL_DERBOT.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=finalderbot.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/finalderbot.x.tar
